363b29674731fd8297029030ecfb7431e2e0d561
imen bedoui <imenbedoui25@gmail.com>
2023-04-03 15:42:17 +0100

    first step with node

---
+++ server.js
@@ -0,0 +1,17 @@
+const http= require ('http')
+
+
+const server = http.createServer((req,res)=>{
+    res.write('hello\n')
+    res.write('hello')
+    res.write('hello')
+    res.write('imen')
+    res.write('hello\n')
+    res.write('bedoui')
+
+    res.end()
+})
+
+server.listen(5000,()=>{
+    console.log('server runnung')
+})
